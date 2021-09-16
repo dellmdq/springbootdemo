768e2a5b8ffb18c759c1e39c2ccc57f8fe234e36
dellmdq <erikdell@gmail.com>
2021-09-16 23:22:05 +0200

    Springboot Section Finished

---
+++ 01-spring-boot-demo/src/test/java/com/luv2code/myCoolApp/AppTest.java
@@ -0,0 +1,20 @@
+package com.luv2code.myCoolApp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
